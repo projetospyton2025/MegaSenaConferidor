e4c118c7ceb378ea12b16f7e0086cee38850fa15
Marcio Maia <marciofernando.maia@yahoo.com.br>
2025-01-24 15:59:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -481,7 +481,7 @@ def exportar_jogos_sorteados(data, formato):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 """ LOCALHOST
